db221323f8523f8e545a1ac52900acd9db257301
Austin Vern Songer <austinvernsonger@outlook.com>
2016-08-29 11:23:25 -0500

    Cleaned Up Code

--- add_shortcode_tynimce.php
+++ add_shortcode_tynimce.php
@@ -1,38 +1,43 @@
 <?php
-
-if(!class_exists('PFL_Insert_Html_TinyMCESelector')):
-
-class PFL_Insert_Html_TinyMCESelector{
-	var $buttonName = 'PFL_ihs_snippet_selector';
-	function addSelector(){
-		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
-			return;
-
-	   // Add only in Rich Editor mode
-	    if ( get_user_option('rich_editing') == 'true') {
-	      add_filter('mce_external_plugins', array($this, 'registerTmcePlugin'));
-	      add_filter('mce_buttons', array($this, 'registerButton'));
-	    }
-	}
-
-	function registerButton($buttons){
-		array_push($buttons, "separator", $this->buttonName);
-		return $buttons;
-	}
-
-	function registerTmcePlugin($plugin_array){
-		$plugin_array[$this->buttonName] =get_site_url() . '/index.php?wp_ihs=editor_plugin_js';
-		if ( get_user_option('rich_editing') == 'true')
-		 	//var_dump($plugin_array);
-		return $plugin_array;
-	}
-}
-
+if (!class_exists('PFL_Insert_Html_TinyMCESelector')):
+    class PFL_Insert_Html_TinyMCESelector
+    {
+        var $buttonName = 'PFL_ihs_snippet_selector';
+        function addSelector()
+        {
+            if (!current_user_can('edit_posts') && !current_user_can('edit_pages'))
+                return;
+            // Add only in Rich Editor mode
+            if (get_user_option('rich_editing') == 'true') {
+                add_filter('mce_external_plugins', array(
+                    $this,
+                    'registerTmcePlugin'
+                ));
+                add_filter('mce_buttons', array(
+                    $this,
+                    'registerButton'
+                ));
+            }
+        }
+        function registerButton($buttons)
+        {
+            array_push($buttons, "separator", $this->buttonName);
+            return $buttons;
+        }
+        function registerTmcePlugin($plugin_array)
+        {
+            $plugin_array[$this->buttonName] = get_site_url() . '/index.php?wp_ihs=editor_plugin_js';
+            if (get_user_option('rich_editing') == 'true')
+            //var_dump($plugin_array);
+                return $plugin_array;
+        }
+    }
 endif;
-
-if(!isset($shortcodesPFLEH)){
-	$shortcodesPFLEH = new PFL_Insert_Html_TinyMCESelector();
-	add_action('admin_head', array($shortcodesPFLEH, 'addSelector'));
+if (!isset($shortcodesPFLEH)) {
+    $shortcodesPFLEH = new PFL_Insert_Html_TinyMCESelector();
+    add_action('admin_head', array(
+        $shortcodesPFLEH,
+        'addSelector'
+    ));
 }
-
 ?>
--- direct_call.php
+++ direct_call.php
@@ -1,17 +1,16 @@
 <?php
-function PFL_ihs_plugin_query_vars($vars) {
-	$vars[] = 'wp_ihs';
-	return $vars;
+function PFL_ihs_plugin_query_vars($vars)
+{
+    $vars[] = 'wp_ihs';
+    return $vars;
 }
 add_filter('query_vars', 'PFL_ihs_plugin_query_vars');
-
-
-function PFL_ihs_plugin_parse_request($wp) {
-	/*confirmation*/
-	if (array_key_exists('wp_ihs', $wp->query_vars) && $wp->query_vars['wp_ihs'] == 'editor_plugin_js') {
-		require( dirname( __FILE__ ) . '/editor_plugin.js.php' );
-		die;
-	}
-	
+function PFL_ihs_plugin_parse_request($wp)
+{
+    /*confirmation*/
+    if (array_key_exists('wp_ihs', $wp->query_vars) && $wp->query_vars['wp_ihs'] == 'editor_plugin_js') {
+        require(dirname(__FILE__) . '/editor_plugin.js.php');
+        die;
+    }
 }
 add_action('parse_request', 'PFL_ihs_plugin_parse_request');
